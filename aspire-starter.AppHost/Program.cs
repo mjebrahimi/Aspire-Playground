b9df2cc2fa160d51514b8c021d93eeaa4523472c
Mohammad Javad Ebrahimi <mj.ebrahimi72@gmail.com>
2024-07-03 17:03:31 +0330

    Use local sql/redis instead of container ones

--- aspire-starter.AppHost/Program.cs
+++ aspire-starter.AppHost/Program.cs
@@ -1,7 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var redis = builder.AddRedis("redis");
-var sqlserver = builder.AddSqlServer("sqlserver").AddDatabase("database");
+var sqlserver = builder.AddSqlServer("database");
 
 builder.AddProject<Projects.aspire_starter_ApiService>("apiservice")
     .WithReference(redis)
---
+++ aspire-starter.AppHost/ResourceBuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿public static class ResourceBuilderExtensions
+{
+    public static IResourceBuilder<IResourceWithConnectionString> AddSqlServer(this IDistributedApplicationBuilder builder, string name)
+    {
+        var localSqlConnection = builder.Configuration["LocalSqlServerConnection"];
+
+        if (string.IsNullOrWhiteSpace(localSqlConnection) is false)
+        {
+            //Connect to local SqlServer instead of container SqlServer for faster running/debugging the project
+            builder.Configuration[$"ConnectionStrings:{name}"] = localSqlConnection;
+            return builder.AddConnectionString(name);
+        }
+
+        return SqlServerBuilderExtensions.AddSqlServer(builder, "sqlserver").AddDatabase(name);
+    }
+
+    public static IResourceBuilder<IResourceWithConnectionString> AddRedis(this IDistributedApplicationBuilder builder, string name)
+    {
+        var localRedisConnection = builder.Configuration["LocalRedisConnection"];
+
+        if (string.IsNullOrWhiteSpace(localRedisConnection) is false)
+        {
+            //Connect to local Redis instead of container Redis for faster running/debugging the project
+            builder.Configuration[$"ConnectionStrings:{name}"] = localRedisConnection;
+            return builder.AddConnectionString(name);
+        }
+
+        return RedisBuilderExtensions.AddRedis(builder, name);
+    }
+}
